afa7c1850c192fdff178e7ba5d9e4fcf3682fb98
Satish <satishk.koya@gmail.com>
2025-03-04 11:57:55 +0530

    Allow the add method to handle any amount of numbers.

--- test_string_addition.py
+++ test_string_addition.py
@@ -7,3 +7,8 @@ class TestStringMethods(unittest.TestCase):
     ##Create a simple String calculator with a method signature like this:
     def test_string_caluclator(self):
         self.assertEqual(string_addition("10,9"), 19)
+
+
+    ##Allow the add method to handle any amount of numbers.:
+    def test_string_caluclator(self):
+        self.assertEqual(string_addition("10,9,20,30,40,50"), 159)
