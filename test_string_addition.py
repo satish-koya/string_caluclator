5763167c25bacd83c115a45ca96ed0bdb1c6ea07
Satish <satishk.koya@gmail.com>
2025-03-04 12:06:19 +0530

    Support different delimiters

--- test_string_addition.py
+++ test_string_addition.py
@@ -17,3 +17,9 @@ class TestStringMethods(unittest.TestCase):
     #Allow the add method to handle new lines between numbers (instead of commas). ("1\n2,3" should return 6)
     def test_string_caluclator_witn_newline(self):
         self.assertEqual(string_addition("10,20,30,40\n50"), 150)
+
+    #Support different delimiters
+    def test_string_caluclator_witn_diff_delimiter(self):
+       " //[delimiter]\n[numbers…]"
+       self.assertEqual(string_addition("//;\n10;20;30;40;50"), 150)
+       self.assertEqual(string_addition("//.\n10.20.30.40.50"), 150)
